a10b05b91095c791ec48dafb96d2cbf316043072
Agung Setiaji <mragungsetiaji@gmail.com>
2018-04-07 16:32:21 +0700

    Update main.js

--- autoadd/main.js
+++ autoadd/main.js
@@ -1,24 +1,23 @@
-var countOfPagesScrolled = 10;
+var countOfPagesScrolled = 5;
 
 function scrollDown(height, countOfPagesScrolled){
-    scroll(0, document.body.clientHeight);
-    setTimeout(function(){
-        if(height != document.body.clientHeight && countOfPagesScrolled > 0){
-            scrollDown(document.body.clientHeight, --countOfPagesScrolled);
-        }else{
-            return sendRequest();
-        }
-    }, 1500);
+	scroll(0, document.body.clientHeight);
+	setTimeout(function(){
+		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
+			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
+		}else
+			return sendRequest();
+	}, 1500);
 }
 
 function sendRequest(){
-    var contactsNum = 0;
-    jQuery.each($('.buttton-secondary-small'), function(){
-        contactsNum++;
-        $(this).click();
-        console.log($(this).attr('title') + ' ' + contactsNum);
-    });
-    console.log('Baru aja nambahin koneksi sebanyak:' + contactsNum);
+	var contactsNum = 0;
+	jQuery.each( $('.button-secondary-small'), 	function() {
+		contactsNum++;
+		$(this).click();
+		console.log($(this).attr('title') + ' ' + contactsNum);
+	});
+	console.log('Just added contacts: ' + contactsNum);
 }
 
 scrollDown(document.body.clientHeight, countOfPagesScrolled);
